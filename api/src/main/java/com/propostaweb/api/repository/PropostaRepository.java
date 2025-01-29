cfb12b581eb369987040b47d1b8ce70d313731d4
lewy <lewyalves78@gmail.com>
2025-01-29 07:53:21 -0300

    consulta na proposta

--- api/src/main/java/com/propostaweb/api/repository/PropostaRepository.java
+++ api/src/main/java/com/propostaweb/api/repository/PropostaRepository.java
@@ -3,5 +3,9 @@ package com.propostaweb.api.repository;
 import com.propostaweb.api.entity.PropostaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PropostaRepository extends JpaRepository<PropostaEntity, Long> {
+
+    List<PropostaEntity> findAllByIntegradaIsFalse();
 }
